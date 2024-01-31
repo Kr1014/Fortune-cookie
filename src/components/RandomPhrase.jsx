e612bafb2475583de6066eadbc9872b10392b50f
Kelvin <kelvinrodriguez1409@gmail.com>
2024-01-30 21:55:56 -0600

    changes

--- src/components/RandomPhrase.jsx
+++ src/components/RandomPhrase.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { RiDoubleQuotesR } from "react-icons/ri";
+const RandomPhrase = ({ quote }) => {
 
-const RandomPhrase = ({quote}) => {
-
-    const {author, phrase} = quote
+  const { author, phrase } = quote
 
   return (
     <div className='container-phrase'>
       <div className="content-phrase-author">
         <p className='phrase'>{phrase}</p>
-        <p className='author'>Fuente : {author} <i className='icon-quotes'><RiDoubleQuotesR/></i></p>
+        <p className='author'>Fuente : {author}</p>
       </div>
     </div>
   )
